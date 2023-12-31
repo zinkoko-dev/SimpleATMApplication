c5689aa5bb8e99e51c892096132850dd581a0e42
Zin Ko Ko <zinkoko@acedatasystems.com>
2023-12-31 21:47:28 +0630

    [Complete] _Layout Setting

---
+++ SimpleATMApplication/Controllers/AtmActionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleATMApplication.Controllers;
+
+public class AtmActionController : Controller
+{
+    // GET
+    [ActionName("Index")]
+    public IActionResult AtmActionIndex()
+    {
+        return View("AtmActionIndex");
+    }
+}
