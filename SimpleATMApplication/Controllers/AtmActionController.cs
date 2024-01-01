4b26d2ae71c4e089ee0d68182e648acc9265322b
Zin Ko Ko <zinkoko@acedatasystems.com>
2024-01-01 16:56:31 +0630

    [Update] contoller

--- SimpleATMApplication/Controllers/AtmActionController.cs
+++ SimpleATMApplication/Controllers/AtmActionController.cs
@@ -1,13 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleATMApplication.EFDbContext;
+using SimpleATMApplication.Models;
 
 namespace SimpleATMApplication.Controllers;
 
 public class AtmActionController : Controller
 {
+    private readonly AppDbContext _appDbContext;
+
+    public AtmActionController(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
     // GET
     [ActionName("Index")]
     public IActionResult AtmActionIndex()
     {
         return View("AtmActionIndex");
+    }
+
+    [HttpPost]
+    public IActionResult Login(CardHolderResponseModel reqModel)
+    {
+        CardHolderDataModel cardHolderDataModel = _appDbContext.GetCardHolderByCardNumAndPin(reqModel.CardNumber, reqModel.Pin);
+        if (cardHolderDataModel is null)
+        {
+            TempData["Message"] = "Login Fail !! Please Enter Again";
+            return Redirect("/");
+        }
+        return View("AtmActionHome", cardHolderDataModel);
+    }
+
+    [HttpPost]
+    public IActionResult Deposit([FromBody] CardHolderResponseModel model)
+    {
+        if (model != null)
+        {
+            double amt = model.DepositAmt;
+            return Ok("Deposit successful");
+        }
+        else
+        {
+            return BadRequest("Invalid data received");
+        }
     }
 }
